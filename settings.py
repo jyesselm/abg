cd9d5cfeb2162f837bed50ddcdb972595f6884da
Joe Yesselman <jyesselm@unl.edu>
2020-04-29 11:57:19 -0500

    rearranged package to distribution

--- settings.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def get_lib_path():
-    file_path = os.path.realpath(__file__)
-    spl = file_path.split("/")
-    base_dir = "/".join(spl[:-1])
-    return base_dir
-
-AFORM_HELIX_PDB_PATH = get_lib_path() + "/resources/iAformRNA.pdb"
